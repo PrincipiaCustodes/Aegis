edf1240615dd38f72a70e27dcceaffa5d132a4b8
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-04-30 11:40:10 +0500

    add biometric in MainActivity

--- Egida/app/src/main/java/com/example/egida/MainActivity.java
+++ Egida/app/src/main/java/com/example/egida/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.egida;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import androidx.biometric.BiometricPrompt;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
+
+    // UI
+    Button authButton;
+    TextView authStatus;
+
+    // Biometric
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authButton = findViewById(R.id.authBtn);
+        authStatus = findViewById(R.id.authStat);
+
+        executor = ContextCompat.getMainExecutor(getApplicationContext());
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new androidx.biometric.BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                authStatus.setText("Error: " + errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                // auth successful
+                super.onAuthenticationSucceeded(result);
+                authStatus.setText("Ok");
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                // auth failed
+                super.onAuthenticationFailed();
+                authStatus.setText("Authentication failed");
+            }
+        });
+
+        // setup auth dialog
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric authentication")
+                .setSubtitle("Fingerprint or face")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        authButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // show dialog
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
     }
 }
