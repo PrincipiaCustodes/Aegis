624656afebb43d013d66592ba51cd0b7235b6826
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-05-12 19:12:54 +0500

    add signUp activity

---
+++ Egida/app/src/main/java/com/example/egida/SignUpActivity.java
@@ -0,0 +1,28 @@
+package com.example.egida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private Button signUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        signUpButton = findViewById(R.id.signup_btn);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
