9ebb3f5cc37ea87a6f230e3021f433f8eebc56db
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-05-15 14:23:00 +0500

    add custom alert dialog

--- Egida/app/src/main/java/com/example/egida/SignUpPasswordFragment.java
+++ Egida/app/src/main/java/com/example/egida/SignUpPasswordFragment.java
@@ -2,13 +2,16 @@ package com.example.egida;
 
 import androidx.fragment.app.Fragment;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -19,9 +22,13 @@ public class SignUpPasswordFragment extends Fragment {
 
     private ImageView nextButton;
     private EditText password;
+    private Button alertDialogOkButton;
+    private Button alertDialogNoButton;
 
     private SharedPreferences sharedPref;
     final private String PREF_TAG = "password";
+
+    Dialog dialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
@@ -32,6 +39,34 @@ public class SignUpPasswordFragment extends Fragment {
 
         nextButton = view.findViewById(R.id.next_btn);
         password = view.findViewById(R.id.password_input_textField);
+
+        dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.custom_alertdialog_layout);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            dialog.getWindow().setBackgroundDrawable(getActivity().getDrawable(R.drawable.custom_alertdialog_background_inset));
+        }
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);   // устанавливает можно ли отменить диалог клавишей "назад"
+        dialog.getWindow().getAttributes().windowAnimations = R.style.custom_alertdialog_animation; // анимация для диалога
+
+        alertDialogOkButton = dialog.findViewById(R.id.alertDialog_ok);
+        alertDialogNoButton = dialog.findViewById(R.id.alertDialog_no);
+
+        alertDialogOkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        alertDialogNoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
