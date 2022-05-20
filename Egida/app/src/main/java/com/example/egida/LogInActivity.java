e0f456999e34049409cbd9a6e89bbc519aa43ee0
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-05-20 14:40:17 +0500

    при входе в приложение пользователю будет предложено использовать биометрию, если это возможно. Маленькте изменение в регистрации. Решена проблема: при закрытии диалога биометрии пароль не работает

--- Egida/app/src/main/java/com/example/egida/LogInActivity.java
+++ Egida/app/src/main/java/com/example/egida/LogInActivity.java
@@ -1,6 +1,10 @@
 package com.example.egida;
 
+import static androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG;
+import static androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -21,6 +25,8 @@ public class LogInActivity extends AppCompatActivity {
     private TextView welcomeText;
     private EditText password;
     private ImageView nextButton;
+
+    private CustomAlertDialog customAlertDialog1;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -46,28 +52,66 @@ public class LogInActivity extends AppCompatActivity {
             };
             biometrics.biometricsPrompt(this);
         } else {
-            nextButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // сравниваем хеш введённого пароля с сохранённым
-                    try {
-                        if((new ShaEncoder(password.getText().toString()).sha256EncodeInput())
-                                .equals(SharedPrefs.getPASSWORD(getApplicationContext()))) {
-                            Toast.makeText(getApplicationContext(), getString(R.string.authentication_successful),
-                                    Toast.LENGTH_SHORT).show();
+            canUseBiometrics();
+        }
 
-                            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                        } else {
-                            Toast.makeText(getApplicationContext(), getString(R.string.wrong_password),
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    } catch (NoSuchAlgorithmException e) {
-                        e.printStackTrace();
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // сравниваем хеш введённого пароля с сохранённым
+                try {
+                    if((new ShaEncoder(password.getText().toString()).sha256EncodeInput())
+                            .equals(SharedPrefs.getPASSWORD(getApplicationContext()))) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.authentication_successful),
+                                Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), getString(R.string.wrong_password),
+                                Toast.LENGTH_SHORT).show();
                     }
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
                 }
-            });
+            }
+        });
+    }
+
+
+    private void canUseBiometrics(){
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                // alertDialog, о том, что ПРЯМО СЕЙЧАС пользователь может использовать билметрические данные
+                customAlertDialog1 = new CustomAlertDialog(this) {
+                    @Override
+                    public void positiveAction() {
+                        SharedPrefs.setBIOMETRICS_STATUS(getApplicationContext(), getString(R.string.biometrics_status_use));
+
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void negativeAction() {
+                        getDialog().dismiss();
+                    }
+                };
+
+                customAlertDialog1.setAlertDialogImageId(R.drawable.icon);
+                customAlertDialog1.setNewAlertDialogTittle(getString(R.string.about_biometrics_alertDialog_status_can_use));
+                customAlertDialog1.setNewAlertDialogDescription(getString(R.string.about_biometrics_alertDialog_status_description));
+                customAlertDialog1.setNewAlertDialogQuestion(getString(R.string.about_biometrics_alertDialog_status_question));
+                customAlertDialog1.setNewAlertDialogOkButton(getString(R.string.about_biometrics_alertDialog_status_ok));
+                customAlertDialog1.setNewAlertDialogNoButton(getString(R.string.about_biometrics_alertDialog_status_no));
+                customAlertDialog1.setupAlertDialogSettings();
+                customAlertDialog1.getDialog().show();
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                break;
         }
     }
 }
