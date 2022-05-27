1d879f166808b0c91f71e0580c52f6ef5be6e55c
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-05-28 00:01:19 +0500

    add simple server

--- Egida/app/src/main/java/com/example/egida/ShareFragment.java
+++ Egida/app/src/main/java/com/example/egida/ShareFragment.java
@@ -1,25 +1,47 @@
 package com.example.egida;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 public class ShareFragment extends Fragment {
 
     Button genButton;
     ImageView qrCode;
+    Switch serverState;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,26 +53,70 @@ public class ShareFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_share, container, false);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         genButton = view.findViewById(R.id.gen_qr_code_btn);
         qrCode = view.findViewById(R.id.qr_code);
+        serverState = view.findViewById(R.id.switch1);
 
         genButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
-                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-                try {
-                    BitMatrix bitMatrix = multiFormatWriter.encode("https://github.com/journeyapps/zxing-android-embedded",
-                            BarcodeFormat.QR_CODE, 350, 350);
+                createQrCode();
+                new Server(8080, "/storage/emulated/Test").start();
+            }
+        });
 
-                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-                    qrCode.setImageBitmap(bitmap);
-                } catch (WriterException e) {
-                    e.printStackTrace();
+        serverState.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    new Server(8080, "/storage/emulated/Test").start();
                 }
             }
         });
 
+
         return view;
+    }
+
+    private void createQrCode(){
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode("https://github.com/journeyapps/zxing-android-embedded",
+                    BarcodeFormat.QR_CODE, 350, 350);
+
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            qrCode.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class Server {
+
+        private final int port;
+        private final String directory;
+
+        public Server(int port, String directory) {
+            this.port = port;
+            this.directory = directory;
+        }
+
+        void start() {
+            try(ServerSocket server = new ServerSocket(this.port)) {
+                while (true) {
+                    Socket socket = server.accept();
+                    Thread thread = new Thread(new MyHandler(socket, this.directory));
+                    thread.start();
+                }
+
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
