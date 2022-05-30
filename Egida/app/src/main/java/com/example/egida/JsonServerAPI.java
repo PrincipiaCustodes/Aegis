d42df0e95aa1f04216b7b52f31a03a4cc6c06444
PervuhinRoman <rg.pervuhin5198@gmail.com>
2022-05-30 22:32:17 +0500

    last commit before merge

--- Egida/app/src/main/java/com/example/egida/JsonServerAPI.java
+++ Egida/app/src/main/java/com/example/egida/JsonServerAPI.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface JsonServerAPI {
     @GET("posts")
     Call<List<JsonFile>> getPosts();
+
+    @POST
+    Call<ServerPassword> sendPassword();
 }
