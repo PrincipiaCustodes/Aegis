d1b28299f7429d8da962724bf6e51c9e8a6d9c53
DpoofikD <konarevms@gmail.com>
2022-05-31 20:28:37 +0500

    keys.json

--- Egida/app/src/main/java/com/example/egida/Keys.java
+++ Egida/app/src/main/java/com/example/egida/Keys.java
@@ -1,28 +1,78 @@
 package com.example.egida;
 
+import android.os.Build;
 import android.util.Pair;
 
+import androidx.annotation.RequiresApi;
+
+import com.google.gson.Gson;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Keys {
 
-    public class Data {
-        private Map<String, Pair<String, String>> keys;
+        private static Map<String, Pair<String, String>> keys;
 
-        public Data() {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        public Keys() throws IOException {
             String dirPath = "/data/data/com.example.egida/encrypted_files/";
-            File endryptedFiles = new File(dirPath);
-            if (!endryptedFiles.exists())
-                endryptedFiles.mkdirs();
-            File file = new File("/data/data/com.example.egida/encrypted_files/", "keys.json");
-            if (!file.exists()) {
-                
+            File encryptedFiles = new File(dirPath);
+            if (!encryptedFiles.exists())
+                encryptedFiles.mkdirs();
+            File file = new File(dirPath, "keys.json");
+            keys = new HashMap<>();
+
+            if (file.exists()) {
+                recoverFromJson();
             }
+            else {
+                saveToJson();
+            }
+        }
+
+        public void saveToJson() {
+            File file = new File("/data/data/com.example.egida/encrypted_files/", "keys.json");
+            Gson gson = new Gson();
+            String json = gson.toJson(keys);
+            FileOutputStream stream = null;
+            try {
+                stream = new FileOutputStream(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                stream.write(json.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        public void recoverFromJson() throws IOException {
+            File file = new File("/data/data/com.example.egida/encrypted_files/", "keys.json");
+            Gson gson = new Gson();
+            byte[] encoded = Files.readAllBytes(Paths.get("/data/data/com.example.egida/encrypted_files/keys.json"));
+            String json = new String(encoded, StandardCharsets.US_ASCII);
+            keys = gson.fromJson(json, HashMap.class);
         }
 
         public void setValues(String name, String key, String extension) {
             keys.put(name, new Pair<>(key, extension));
+            saveToJson();
         }
 
         public String getDecipherKey(String name, String key) {
@@ -32,7 +82,5 @@ public class Keys {
         public String getDecipherExtension(String name, String key) {
             return keys.get(name).second;
         }
-    }
-
 
 }
--- Egida/app/src/main/java/com/example/egida/SplashScreen.java
+++ Egida/app/src/main/java/com/example/egida/SplashScreen.java
@@ -1,18 +1,26 @@
 package com.example.egida;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+
+import java.io.IOException;
 
 public class SplashScreen extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Keys keys = new Keys ();
-        Keys.Data data = keys.new Data();
+        try {
+            Keys keys = new Keys ();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         SharedPrefs.setFIRST_START(this, true);
 
